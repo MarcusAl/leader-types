81b134502ed67b5b6a29e5b217fc4280b7f060dc
MarcusA <emkay1993@gmail.com>
2021-07-04 15:32:41 +0100

    Delete env.js

--- public/env.js
+++
@@ -1,5 +0,0 @@
-window.env = {
-  "API_ENDPNT": "https://characterscope-engineering-recruitment.cdn.prismic.io/api/v2",
-  "TOKEN": "MC5ZRzhYTkJNQUFDSUFpa09f.77-9Tg3vv70B77-977-9GSzvv70V77-9GBdB77-977-9HRUS77-9F--_vVfvv73vv706ARDvv73vv73vv70",
-  "DOC_ID": "YG76KxMAACEAiboY"
-};
